e160f5622c146dcc11e9b5a8166d80e765f48e65
ty <phuan167@gmail.com>
2024-11-23 18:24:10 +0700

    feat: create user info

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -1,6 +1,7 @@
 
 import { Router } from "express";
-import { login, signup } from "../controllers/AuthController.js";
+import { login, signup, getUserInfo} from "../controllers/AuthController.js";
+import { verifyTokens } from "../middlewares/AuthMiddlewares.js";
 
 
 
@@ -8,5 +9,6 @@ const authRoutes = Router();
 
 authRoutes.post('/signup', signup);
 authRoutes.post('/login', login);
+authRoutes.get('/user-info',verifyTokens,getUserInfo);
 
 export default authRoutes;
