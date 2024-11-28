f33353670d629dc12bcbf5aa83f62658ee33b398
ty <phuan167@gmail.com>
2024-11-28 16:45:19 +0700

    feat:create logout

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -1,6 +1,6 @@
 
 import { Router } from "express";
-import { login, signup, getUserInfo, updateProfile, addProfileImage, removeProfileImage} from "../controllers/AuthController.js";
+import { login, signup, getUserInfo, updateProfile, addProfileImage, removeProfileImage, logout} from "../controllers/AuthController.js";
 import { verifyTokens } from "../middlewares/AuthMiddlewares.js";
 import multer from 'multer'
 
@@ -13,5 +13,9 @@ authRoutes.post('/login', login);
 authRoutes.get('/user-info',verifyTokens,getUserInfo);
 authRoutes.post('/update-profile',verifyTokens,updateProfile);
 authRoutes.post('/add-profile-image',verifyTokens,upload.single('profile-image'), addProfileImage);
-authRoutes.delete('/remove-profile-image', verifyTokens, removeProfileImage)
+authRoutes.delete('/remove-profile-image', verifyTokens, removeProfileImage);
+authRoutes.post('/logout', verifyTokens, logout);
+
+
+
 export default authRoutes;
